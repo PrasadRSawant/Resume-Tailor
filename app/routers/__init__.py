b7b9e13a18fb95677ff2e683316d90eadcf9b5ab
Prasad R Sawant <prasadsawant1618@gmail.com>
2025-09-20 16:49:54 +0530

    Create app/routers/__init__.py to make routers a package

---
+++ app/routers/__init__.py
@@ -0,0 +1,4 @@
+# app/routers/__init__.py
+# This file makes the "routers" directory a Python package.
+# It can be used to import sub-routers or define package-wide variables.
+# For now, it's left empty to simply establish the package structure.
