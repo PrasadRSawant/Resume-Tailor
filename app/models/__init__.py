38063d765eeb5b8ade103d516336dbe5da96f7b1
Prasad R Sawant <prasadsawant1618@gmail.com>
2025-09-20 16:49:13 +0530

    Create app/models/__init__.py to make models a package

---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+# app/models/__init__.py
+# This file makes "models" a Python package.
+# You can import models directly from app.models.* after defining them here or in separate files within this directory.
